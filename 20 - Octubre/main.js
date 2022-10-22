9c7760cc704347cddb4d21e586d57177ec30b5c3
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2022-10-21 20:05:32 -0500

    Create main.js

---
+++ 20 - Octubre/main.js
@@ -0,0 +1,50 @@
+// Ejercicio1
+
+//let arreglo = [1,4,15,20,8,5,10,11,54];
+//let NumMayor = 0;
+
+//for (i = 0; i < arreglo.length; i++){
+//  if (arreglo[i] >= NumMayor){
+//    NumMayor = arreglo[i];
+//  }
+//}
+
+//console.log(NumMayor);
+
+//Ejercicio2
+
+//let palabra = "Otto";
+//let palindromo = "";
+
+//palabra = palabra.toLocaleLowerCase().replace(/\ s/g,"");
+
+//for (let i = palabra.length -1; i >= 0; i--) {
+//     palindromo += palabra[i];
+//}
+
+
+//console.log("Esta palabra es un palindromo?", palabra == palindromo);
+
+//Ejercicio3 no lo logre, pero un merito por el intento xd 
+
+//let array = [[1,0,0],[0,1,1],[0,1,0]]
+//let ceros = 0;
+
+//for (let i = 0; i <= array.length; i++){
+//  if (array[i]){
+//    ceros++;
+//  }
+//}
+
+//console.log("La cantidad de ceros es de " + ceros);
+
+//Ejercicio5
+
+//for(let i=1; i <=6; ++i){
+//  let resultado = '';
+
+//  for (let j = 1; j <= i; ++j){
+//      resultado += '* '    
+//  }
+//  console.log(resultado);
+//}
